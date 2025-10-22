a843977a4e3ec29d057c486008a850aa95e2f30d
pietroid <pietrodomingues@gmail.com>
2025-10-22 18:54:32 -0300

    adding flutter project

---
+++ lib/app/view/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:workflow_lab/counter/counter.dart';
+import 'package:workflow_lab/l10n/l10n.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const CounterPage(),
+    );
+  }
+}
