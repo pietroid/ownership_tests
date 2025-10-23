f630cbaf456e5d736a61038da76be7ebede2f3c9
pietroid <pietrodomingues@gmail.com>
2025-10-23 11:36:23 -0300

    forgot es

--- .dart_tool/flutter_gen/gen_l10n/app_localizations_es.dart
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_es.dart
@@ -9,5 +9,5 @@ class AppLocalizationsEs extends AppLocalizations {
   AppLocalizationsEs([String locale = 'es']) : super(locale);
 
   @override
-  String get counterAppBarTitle => 'Contador';
+  String get counterAppBarTitle => 'Contador con modificación';
 }
