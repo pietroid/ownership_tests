5b99af98c33355c0b1af4a244d540cd31878c846
pietroid <pietrodomingues@gmail.com>
2025-10-23 11:36:04 -0300

    adding counter

--- .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -9,5 +9,5 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get counterAppBarTitle => 'Counter';
+  String get counterAppBarTitle => 'Counter with modification';
 }
