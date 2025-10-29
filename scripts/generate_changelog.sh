8e86270034817032f1591195aea565f0a9b54801
pietroid <pietrodomingues@gmail.com>
2025-10-29 17:08:44 -0300

    improving workflows

---
+++ scripts/generate_changelog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Usage: ./generate_changelog.sh <base_branch>
+# Example: ./generate_changelog.sh main
+BASE_BRANCH="${2:-main}"
+HEAD_BRANCH="${1:-HEAD}"
+
+git fetch origin "$BASE_BRANCH"
+git fetch origin "$HEAD_BRANCH"
+
+git log origin/"$BASE_BRANCH"..origin/"$HEAD_BRANCH" --pretty=format:"%s" \
+    | grep -v -E '^(Release: |[Mm]erge)' > changelog.txt
