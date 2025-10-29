773e1c0d7481cea65b509d793e658666f6529888
pietroid <pietrodomingues@gmail.com>
2025-10-29 18:15:55 -0300

    improving

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -9,4 +9,6 @@ git fetch origin "$BASE_BRANCH"
 git fetch origin "$HEAD_BRANCH"
 
 git log origin/"$BASE_BRANCH"..origin/"$HEAD_BRANCH" --pretty=format:"%s" \
-    | grep -v -E '^(Release: |[Mm]erge)' > changelog.txt
+    | grep -v -E '^(Release: |[Mm]erge)' \
+    | sed 's/^/- /' > changelog.txt
+    
